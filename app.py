2f94e1c7a72c9e52adf3067e2b3f69e24a1e2879
jcat124 <89937640+jcat124@users.noreply.github.com>
2022-02-15 21:40:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -31,14 +31,27 @@ def how_page():
 @app.route("/instructions")
 def instructions_page():
     windows_defender = os.path.join(app.config['UPLOAD_FOLDER'], 'windows_defender.jpg')
-    first = 'Open the website https://jcat124.github.io/website/.'
+    right_or_wrong_face = os.path.join(app.config['UPLOAD_FOLDER'], 'right_or_wrong_face.png')
+    right_or_wrong_way = os.path.join(app.config['UPLOAD_FOLDER'], 'right_or_wrong_way.png')
+    proper_detection = os.path.join(app.config['UPLOAD_FOLDER'], 'proper_detection.png')
+    first = 'Open the website https://anti-drowsy-driving-plus.herokuapp.com/.'
     second = 'Click on “Home”, then click on “Download”.'
     third = 'Download version 1 if you only want drowsiness detection (ADD).'
     fourth = 'Download version 2 if you want drowsiness detection plus interaction (ADD+).'
     fifth = 'Open the file.'
     sixth = 'If it shows the picture below, then click “More info”, then click “Run anyway”.'
     seventh = 'The desktop app will be installed and then you can use it.'
-    return render_template("pages/instructions.html", first=first, second=second, third=third, fourth=fourth, fifth=fifth, sixth=sixth, seventh=seventh, windows_defender=windows_defender)
+    proper_one = 'Proper lighting: Enough lighting coming to face from the front, not from behind.'
+    proper_two = 'Proper camera angle: The camera should be right in front of you.'
+    proper_three = '''
+    Proper detection: The app is ready to use if the green marks around your eyes and mouth closely follow your eyes’ and mouth’s movements, that is if you close your eyes, the green circle around your eye should also simultaneously go flat/ close; and when you open your mouth—like yawning, the green marks around your mouth should also widen up/ open. And, if you do steps 1 and 2 correctly, eyes and mouth proper detection should not be the problem.
+    '''
+    proper_four = 'Interaction: When asked, say “yes” or “no” clearly.'
+    return render_template("pages/instructions.html", first=first, second=second, third=third, fourth=fourth,
+                           fifth=fifth, sixth=sixth, seventh=seventh, windows_defender=windows_defender,
+                           proper_one=proper_one, right_or_wrong_face=right_or_wrong_face, proper_two=proper_two,
+                           right_or_wrong_way=right_or_wrong_way, proper_three=proper_three,
+                           proper_detection=proper_detection, proper_four=proper_four)
 
 
 @app.route("/troubleshoot")
