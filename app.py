d8c2735b89b58ef51e88939593cf1525a6ae44d9
jcat124 <89937640+jcat124@users.noreply.github.com>
2022-02-19 19:39:54 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, render_template
 import os
+# from PIL import Image
+import cv2
 
 DROWSY_FOLDER = os.path.join('static', 'img')
 
@@ -9,31 +11,45 @@ app.config['UPLOAD_FOLDER'] = DROWSY_FOLDER
 
 @app.route("/")
 def home_page():
-    drowsy_image = os.path.join(app.config['UPLOAD_FOLDER'], 'drowsy-img.png')
-    return render_template("index.html", drowsy_image=drowsy_image)
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    return render_template("index.html", ADDPlus_icon=ADDPlus_icon)
 
 
 @app.route("/about")
 def about_page():
-    return render_template("pages/about.html")
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    return render_template("pages/about.html", ADDPlus_icon=ADDPlus_icon)
 
 
 @app.route("/download")
 def download_page():
-    return render_template("pages/download.html")
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    return render_template("pages/download.html", ADDPlus_icon=ADDPlus_icon)
 
 
 @app.route("/how")
 def how_page():
-    return render_template("pages/how.html")
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    return render_template("pages/how.html", ADDPlus_icon=ADDPlus_icon)
 
 
 @app.route("/instructions")
 def instructions_page():
-    windows_defender = os.path.join(app.config['UPLOAD_FOLDER'], 'windows_defender.jpg')
-    right_or_wrong_face = os.path.join(app.config['UPLOAD_FOLDER'], 'right_or_wrong_face.png')
-    right_or_wrong_way = os.path.join(app.config['UPLOAD_FOLDER'], 'right_or_wrong_way.png')
-    proper_detection = os.path.join(app.config['UPLOAD_FOLDER'], 'proper_detection.png')
+    # windows_defender = url_for('static', filename='img/windows_defender.jpg')
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    windows_defender = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'windows_defender.jpg')
+    right_or_wrong_face = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'right_or_wrong_face.png')
+    right_or_wrong_way = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'right_or_wrong_way.png')
+    proper_detection = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'proper_detection.png')
     first = 'Open the website https://anti-drowsy-driving-plus.herokuapp.com/.'
     second = 'Click on “Home”, then click on “Download”.'
     third = 'Download version 1 if you only want drowsiness detection (ADD).'
@@ -47,22 +63,40 @@ def instructions_page():
     Proper detection: The app is ready to use if the green marks around your eyes and mouth closely follow your eyes’ and mouth’s movements, that is if you close your eyes, the green circle around your eye should also simultaneously go flat/ close; and when you open your mouth—like yawning, the green marks around your mouth should also widen up/ open. And, if you do steps 1 and 2 correctly, eyes and mouth proper detection should not be the problem.
     '''
     proper_four = 'Interaction: When asked, say “yes” or “no” clearly.'
+    # img = cv2.imread(windows_defender)
+    # width = 440
+    # height = 340
+    # dim = (width, height)
+    # resized_windows_defender = cv2.resize(img, dim)
+    # output_size = (125, 125)
+    # i = Image.open(windows_defender)
+    # resized_windows_defender = i.thumbnail(output_size)
     return render_template("pages/instructions.html", first=first, second=second, third=third, fourth=fourth,
                            fifth=fifth, sixth=sixth, seventh=seventh, windows_defender=windows_defender,
                            proper_one=proper_one, right_or_wrong_face=right_or_wrong_face, proper_two=proper_two,
                            right_or_wrong_way=right_or_wrong_way, proper_three=proper_three,
-                           proper_detection=proper_detection, proper_four=proper_four)
+                           proper_detection=proper_detection, proper_four=proper_four, ADDPlus_icon=ADDPlus_icon
+                           )
 
 
 @app.route("/troubleshoot")
 def troubleshoot_page():
-    return render_template("pages/troubleshoot.html")
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    return render_template("pages/troubleshoot.html", ADDPlus_icon=ADDPlus_icon)
 
 
 @app.route("/why")
 def why_page():
-    return render_template("pages/why.html")
+    car_collide = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'car_collide.png')
+    car_collide1 = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'car_collide1.png')
+    ADDPlus_icon = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'icon.png')
+    return render_template("pages/why.html",
+                           car_collide=car_collide, car_collide1=car_collide1, ADDPlus_icon=ADDPlus_icon)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
